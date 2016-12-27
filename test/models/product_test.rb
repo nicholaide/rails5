3c97653fed7632db697c5bfa275aa63f1cff45d5
Nicholai de Guzman <nic@imac>
2016-12-27 14:47:07 -0600

    finished unit testing

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products # Rails loads all fixtures 
+
   
   test "product attributes must not be empty" do
     product = Product.new
@@ -47,7 +46,24 @@ class ProductTest < ActiveSupport::TestCase
     bad.each do |name|
       assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
-    
   end
+  
+  test "product is not valid without a unique title" do
+    product = Product.new(title:  products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+  
+  test "product is not valid without a unique title, not hardcoding error message" do
+    product = Product.new(title:  products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end  
   
 end
