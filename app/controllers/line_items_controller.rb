b4933d507fec72daa3f1df6ead312efa1c267522
Nicholai de Guzman <nic@imac>
2016-12-30 11:14:49 -0600

    iter F2

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,8 +31,9 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
+        format.js
       else
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
