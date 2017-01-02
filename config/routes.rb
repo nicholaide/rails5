213fa186a588884ca1405af0354cd78178cfa819
Nicholai de Guzman <nic@imac>
2017-01-02 14:24:44 -0600

    finished I2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   resources :users
   resources :orders
   resources :line_items
   resources :carts
+  
   root 'store#index', as: 'store_index'
 
   resources :products do
