6cc99e6d7768132881619745b983a745c3d4f7ee
Nicholai de Guzman <nic@imac>
2017-01-01 21:15:46 -0600

    finished task G

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -67,6 +67,12 @@ class ProductsController < ApplicationController
   
   def who_bought
     @product = Product.find(params[:id])
+    @latest_order = @product.orders.order(:updated_at).last
+    if stale?(@latest_order)
+      respond_to do |format|
+        format.atom
+      end
+    end
   end
 
   private
