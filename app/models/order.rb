bbfb1e0abf636f11813dbb3c0bdd8e8a3e253659
Nicholai de Guzman <nic@imac>
2017-01-02 12:53:08 -0600

    finished H task

--- app/models/order.rb
+++ app/models/order.rb
@@ -13,7 +13,7 @@ class Order < ApplicationRecord
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
       item.cart_id = nil
-      line_items << items
+      line_items << item
     end
   end
     
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
+  fixtures :products
+  
   # test "the truth" do
   #   assert true
   # end
